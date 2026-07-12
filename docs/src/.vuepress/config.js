a09ba4c727fa0da6bfaead47a7016fb782028c00
Ayush P Gupta <ayushpguptaapg@gmail.com>
2026-07-12 18:56:49 +0530

    feat: add lock story

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -69,6 +69,7 @@ export default defineUserConfig({
             "/stories/college",
             "/stories/khudi",
             "/stories/neend",
+            "/stories/taala-chabi",
           ],
         },
         {
